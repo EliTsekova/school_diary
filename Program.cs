3e31324d109887ab5f0c1ca1512ffa172fd621ee
Eli <80881409+EliTsekova@users.noreply.github.com>
2026-05-18 22:11:16 +0300

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using school_diary.Data;
 using school_diary.Models;
 using school_diary.Services;
 using school_diary.Mapping;
+using school_diary.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. Конфигурация на connection string и DbContext
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
                        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
@@ -15,19 +19,67 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+// 2. Добавяме Identity с ваша password policy и уникален email
 builder.Services.AddDefaultIdentity<User>(options =>
     {
+        options.User.RequireUniqueEmail = false;
         options.SignIn.RequireConfirmedAccount = false;
+
+        // Паролна политика
+        options.Password.RequireDigit             = false;
+        options.Password.RequireNonAlphanumeric   = false;
+        options.Password.RequireUppercase         = false;
+        options.Password.RequireLowercase         = false;
+        options.Password.RequiredLength           = 6;
     })
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+// 3. Конфигуриране на cookie за API endpoints
+builder.Services.ConfigureApplicationCookie(opt =>
+{
+    opt.Events.OnRedirectToLogin = ctx =>
+    {
+        if (ctx.Request.Path.StartsWithSegments("/api"))
+        {
+            ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            return Task.CompletedTask;
+        }
+        ctx.Response.Redirect(ctx.RedirectUri);
+        return Task.CompletedTask;
+    };
+
+    opt.Events.OnRedirectToAccessDenied = ctx =>
+    {
+        if (ctx.Request.Path.StartsWithSegments("/api"))
+        {
+            ctx.Response.StatusCode = StatusCodes.Status403Forbidden;
+            return Task.CompletedTask;
+        }
+        ctx.Response.Redirect(ctx.RedirectUri);
+        return Task.CompletedTask;
+    };
+});
+
+// 4. Регистрация на services и DI
 builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<ISchoolService, SchoolService>();
+builder.Services.AddScoped<IDirectorService, DirectorService>();
+builder.Services.AddScoped<ITeacherService, TeacherService>();
+builder.Services.AddScoped<IParentService, ParentService>();
+builder.Services.AddScoped<IGradeService, GradeService>();
+//builder.Services.AddScoped<IUserClaimsPrincipalFactory<User>, AppClaimsPrincipalFactory>();
+builder.Services.AddScoped<IAbsenceService, AbsenceService>();
+builder.Services.AddScoped<ICurriculumService, CurriculumService>();
+builder.Services.AddScoped<IDirectorStatisticsService, DirectorStatisticsService>();
+builder.Services.AddScoped<IAdminStatisticsService, AdminStatisticsService>();
+builder.Services.AddScoped<ISubjectService, SubjectService>();
+builder.Services.AddScoped<IStudentNameService, StudentNameService>();
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
-
 builder.Services.AddControllersWithViews();
 
+// 5. Build и seed
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -36,6 +88,7 @@ using (var scope = app.Services.CreateScope())
     await DbInitializer.SeedAsync(services);
 }
 
+// 6. Middleware pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -46,16 +99,19 @@ else
     app.UseHsts();
 }
 
+app.UseMiddleware<school_diary.Middleware.ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-app.UseAuthentication(); // 🔐
+app.UseAuthentication();
 app.UseAuthorization();
 
+// 7. Роутинг
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
+
