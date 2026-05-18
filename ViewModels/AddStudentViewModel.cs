bbdbe84244deb093eeab11708573fcd621375ec3
Eli <80881409+EliTsekova@users.noreply.github.com>
2026-05-18 22:04:19 +0300

    add ViewModels

---
+++ ViewModels/AddStudentViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace school_diary.ViewModels
+{
+    public class AddStudentViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "School")]
+        public int SelectedSchoolId { get; set; }
+
+        [Required]
+        [Display(Name = "Class")]
+        public string SelectedClassName { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> Schools { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> Classes { get; set; }
+    }
+}
---
+++ ViewModels/EditStudentViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace school_diary.ViewModels
+{
+    public class EditStudentViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password (optional)")]
+        public string? NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Please select a school")]
+        [Display(Name = "School")]
+        public int? SelectedSchoolId { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> Schools { get; set; }
+
+        [Required(ErrorMessage = "Please select a class")]
+        [Display(Name = "Class")]
+        public string SelectedClassName { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> Classes { get; set; }
+    }
+}
