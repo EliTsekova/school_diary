4317dcdd350c4eb9c517797214f935ae4acfb9cc
Eli <80881409+EliTsekova@users.noreply.github.com>
2026-05-18 21:47:36 +0300

    add login logic

---
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using school_diary.Models;
+
+namespace school_diary.Areas.Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<User> _signInManager;
+
+        public LoginModel(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = new();
+
+        public string? ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; } = "";
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; } = "";
+
+            public bool RememberMe { get; set; }
+        }
+
+        public void OnGet(string? returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/RoleRedirect");
+
+            if (!ModelState.IsValid)
+                return Page();
+
+            var result = await _signInManager.PasswordSignInAsync(
+                Input.Email,
+                Input.Password,
+                Input.RememberMe,
+                lockoutOnFailure: false);
+
+            if (result.Succeeded)
+                return LocalRedirect(returnUrl);
+
+            ModelState.AddModelError(string.Empty, "Invalid email or password.");
+
+            return Page();
+        }
+    }
+}
---
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using school_diary.Models;
+
+namespace school_diary.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<User> _signInManager;
+
+        public LogoutModel(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _signInManager.SignOutAsync();
+
+            return Redirect("/Identity/Account/Login");
+        }
+
+        public IActionResult OnGet()
+        {
+            return Redirect("/Identity/Account/Login");
+        }
+    }
+}
