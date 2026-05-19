04a36dacb53b36dda482bdc28ae61f638b1d472e
Eli <80881409+EliTsekova@users.noreply.github.com>
2026-05-19 13:16:39 +0300

    update ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -179,67 +179,34 @@ namespace school_diary.Data
                 .HasIndex(e => new { e.CurriculumId, e.DayOfWeek, e.Period })
                 .IsUnique();
 
-            modelBuilder.Entity<Class>().HasData(
-                new Class { Id = 1, Name = "1A", SchoolId = 1 },
-                new Class { Id = 2, Name = "1B", SchoolId = 1 },
-                new Class { Id = 3, Name = "1C", SchoolId = 1 },
-                new Class { Id = 4, Name = "1D", SchoolId = 1 },
-
-                new Class { Id = 5, Name = "2A", SchoolId = 1 },
-                new Class { Id = 6, Name = "2B", SchoolId = 1 },
-                new Class { Id = 7, Name = "2C", SchoolId = 1 },
-                new Class { Id = 8, Name = "2D", SchoolId = 1 },
-
-                new Class { Id = 9, Name = "3A", SchoolId = 1 },
-                new Class { Id = 10, Name = "3B", SchoolId = 1 },
-                new Class { Id = 11, Name = "3C", SchoolId = 1 },
-                new Class { Id = 12, Name = "3D", SchoolId = 1 },
-
-                new Class { Id = 13, Name = "4A", SchoolId = 1 },
-                new Class { Id = 14, Name = "4B", SchoolId = 1 },
-                new Class { Id = 15, Name = "4C", SchoolId = 1 },
-                new Class { Id = 16, Name = "4D", SchoolId = 1 },
-
-                new Class { Id = 17, Name = "5A", SchoolId = 1 },
-                new Class { Id = 18, Name = "5B", SchoolId = 1 },
-                new Class { Id = 19, Name = "5C", SchoolId = 1 },
-                new Class { Id = 20, Name = "5D", SchoolId = 1 },
-
-                new Class { Id = 21, Name = "6A", SchoolId = 1 },
-                new Class { Id = 22, Name = "6B", SchoolId = 1 },
-                new Class { Id = 23, Name = "6C", SchoolId = 1 },
-                new Class { Id = 24, Name = "6D", SchoolId = 1 },
-
-                new Class { Id = 25, Name = "7A", SchoolId = 1 },
-                new Class { Id = 26, Name = "7B", SchoolId = 1 },
-                new Class { Id = 27, Name = "7C", SchoolId = 1 },
-                new Class { Id = 28, Name = "7D", SchoolId = 1 },
-
-                new Class { Id = 29, Name = "8A", SchoolId = 1 },
-                new Class { Id = 30, Name = "8B", SchoolId = 1 },
-                new Class { Id = 31, Name = "8C", SchoolId = 1 },
-                new Class { Id = 32, Name = "8D", SchoolId = 1 },
-
-                new Class { Id = 33, Name = "9A", SchoolId = 1 },
-                new Class { Id = 34, Name = "9B", SchoolId = 1 },
-                new Class { Id = 35, Name = "9C", SchoolId = 1 },
-                new Class { Id = 36, Name = "9D", SchoolId = 1 },
-
-                new Class { Id = 37, Name = "10A", SchoolId = 1 },
-                new Class { Id = 38, Name = "10B", SchoolId = 1 },
-                new Class { Id = 39, Name = "10C", SchoolId = 1 },
-                new Class { Id = 40, Name = "10D", SchoolId = 1 },
-
-                new Class { Id = 41, Name = "11A", SchoolId = 1 },
-                new Class { Id = 42, Name = "11B", SchoolId = 1 },
-                new Class { Id = 43, Name = "11C", SchoolId = 1 },
-                new Class { Id = 44, Name = "11D", SchoolId = 1 },
-
-                new Class { Id = 45, Name = "12A", SchoolId = 1 },
-                new Class { Id = 46, Name = "12B", SchoolId = 1 },
-                new Class { Id = 47, Name = "12C", SchoolId = 1 },
-                new Class { Id = 48, Name = "12D", SchoolId = 1 }
+            // Seed School
+            modelBuilder.Entity<School>().HasData(
+                new School
+                {
+                    Id = 1,
+                    Name = "Default School",
+                    Address = "Default Address"
+                }
             );
+
+            // Seed Classes
+            var classes = new List<Class>();
+            int id = 1;
+
+            for (int grade = 1; grade <= 12; grade++)
+            {
+                foreach (var letter in new[] { "A", "B", "C", "D" })
+                {
+                    classes.Add(new Class
+                    {
+                        Id = id++,
+                        Name = $"{grade}{letter}",
+                        SchoolId = 1
+                    });
+                }
+            }
+
+            modelBuilder.Entity<Class>().HasData(classes);
         }
     }
 }
